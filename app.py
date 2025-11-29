c5d981e90c52e219449de068643b1cefcd6a3eff
sdnnh8rc97-a11y <sdnnh8rc97@privaterelay.appleid.com>
2025-11-29 14:07:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -80,14 +80,12 @@ async def predict_group_endpoint(payload: GroupPayload):
         "results": results,
     }
 
-# --------------------------------------------------
-# 7. 啟動 API Server（Cloud Run / Colab 都能用）
-# --------------------------------------------------
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=8080,
+        port=8000,
         reload=False
     )
+
