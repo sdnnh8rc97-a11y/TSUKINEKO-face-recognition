970e9f7e454a0d99a4a8a3288848fa3085bac941
sdnnh8rc97-a11y <sdnnh8rc97@privaterelay.appleid.com>
2025-11-29 19:00:36 +0800

    Update classifier_svm.py

--- src/classifier_svm.py
+++ src/classifier_svm.py
@@ -1,13 +1,20 @@
 import pickle
-import numpy as np
+import os
 
-MODEL_PATH = "models/svm_classifier.pkl"
+DEFAULT_SVM_PATH = os.path.join(
+    os.path.dirname(__file__),
+    "models",
+    "svm.pkl"
+)
 
-def load_svm():
-    with open(MODEL_PATH, "rb") as f:
+def load_svm(path: str = DEFAULT_SVM_PATH):
+    """Load SVM model from given path."""
+    with open(path, "rb") as f:
         return pickle.load(f)
+
 
 def svm_predict(model, emb):
     pred = model.predict([emb])[0]
-    conf = max(model.predict_proba([emb])[0])
-    return pred, float(conf)
+    score = model.decision_function([emb])[0]
+    return pred, float(score)
+
